ace8bcef14e2e18dbc587c7ac241da82fc90a679
franko4don <franko4don@gmail.com>
2019-02-05 08:38:11 +0100

    save before renaming app

--- src/App.js
+++ src/App.js
@@ -4,13 +4,51 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import reducers from './redux/reducers';
-import { AsyncStorage, View } from "react-native";
+import { AppState, BackHandler, Alert } from "react-native";
 import { MenuProvider } from 'react-native-popup-menu';
 
 import Router from './Router';
 
 
 class App extends Component {
+    constructor() {
+        super();
+        this.handleAppStateChange = this.handleAppStateChange.bind(this);
+        this.handleBackButton = this.handleBackButton.bind(this);
+    }
+
+    handleBackButton = () => {
+        Alert.alert(
+            'Exit Gclout',
+            'Exiting the application?', [{
+                text: 'Cancel',
+                onPress: () => console.log('Cancel Pressed'),
+                style: 'cancel'
+            }, {
+                text: 'OK',
+                onPress: () => BackHandler.exitApp()
+            },], {
+                cancelable: false
+            }
+        )
+        return true;
+    }
+    componentDidMount() {
+        BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
+        AppState.addEventListener('change', this.handleAppStateChange)
+    }
+
+    componentWillMount() {
+        BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
+        AppState.removeEventListener('change', this.handleAppStateChange)
+    }
+
+    handleAppStateChange(appState) {
+        if (appState === 'background') {
+            console.log("I dey background");
+        }
+    }
+
 
     render() {
 
